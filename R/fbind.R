7d8dcd7cc9c964675270584c46e949cf4ff0e4fe
hatchi28 <aakanksha023@gmail.com>
2020-02-29 09:43:06 -0800

    tried

--- R/fbind.R
+++ R/fbind.R
@@ -1,6 +1,3 @@
-a <- factor(c("character", "hits", "your", "eyeballs"))
-b <- factor(c("but", "integer", "where it", "counts"))
-
 fbind <- function(a, b) {
   factor(c(as.character(a), as.character(b)))
 }
